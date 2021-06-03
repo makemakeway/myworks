90be009455a48fe59abb87f688e0cc01c6c51c58
yeonpark <makemakeway@gmail.com>
2021-06-03 18:00:34 +0900

    level2

---
+++ algorithm/프린터.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    order = deque()
+    for p in priorities:
+        order.append(p)
+        pr
+    return answer
