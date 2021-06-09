e08b9acce97ea83f069f67ae70d48cb33756d9e8
yeonpark <makemakeway@gmail.com>
2021-06-09 19:11:35 +0900

    연습

--- c/자가진단.py
+++ c/자가진단.py
@@ -21,14 +21,12 @@ def solution_0(arr):
 def solution_1(arr):
     answer = []
     set_arr = sorted(set(arr))      #set으로 arr의 중복된 숫자를 없애고, 오름차순 순서로 정렬
-    done = []
-    for nbr in set_arr:             #set_arr의 원소를 순환
-        cnt = arr.count(nbr)      #arr에 있는 nbr의 갯수를 구해 cnt에 저장
-        if cnt > 1:             #cnt가 1보다 크다는것은 중복이 있다는 것
-            answer.append(cnt)  #answer에 cnt를 원소로 추가시켜줌
-            done.append(nbr)    #현재 확인한 숫자를 done에 추가시켜줌
+    for nbr in set_arr:             
+        cnt = arr.count(nbr)        #nbr의 갯수를 구해 cnt에 저장
+        if cnt > 1:                 #cnt가 1보다 크다는것은 중복이 있다는 것
+            answer.append(cnt)      #answer에 cnt를 원소로 추가시켜줌
     if answer == []:
-        answer.append(-1)       #중복이 없을 경우, -1을 원소로 추가시켜줌
+        answer.append(-1)           #중복이 없을 경우, -1을 원소로 추가시켜줌
     return answer
 
 print(solution_0(arr))
